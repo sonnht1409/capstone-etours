f9b7cd297e2b111d57e27f5e9c27ee4f0b48b147
Son Nguyen <sonnht1409@gmail.com>
2017-05-30 20:29:54 +0700

    created connection to database

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-var app = require('express')();
+const app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const io = require('socket.io')(http);
 var port = 8080;
 
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
+
+const sql = require('mssql');
+
+const connection = new sql.ConnectionPool({
+    user: 'etours',
+    password: "$Son01627335534",
+    server: "etours.database.windows.net",
+    driver: 'tedious',
+    database: 'etours',
+    options: {
+        encrypt: true
+    }
+
+
+})
+
+connection.connect(err => {
+    if (err) {
+        console.log("have error");
+        console.log(err)
+    } else {
+        console.log("connect successfully")
+    }
+})
 
 app.get('/', function(req, res) {
     res.sendFile(appDir + '/index.html');
