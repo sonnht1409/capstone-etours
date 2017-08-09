6899058d910695982eb0cd1d411b418926e6ffc2
Son Nguyen <sonnht1409@gmail.com>
2017-08-10 05:05:07 +0700

    update dbconfig

--- dbconfig.js
+++ dbconfig.js
@@ -1,17 +1,32 @@
+/* for local */
+const connection = ({
+    user: 'admin',
+    password: 'evil001',
+    server: 'localhost',
+    driver: 'tedious',
+    database: 'etours',
+    port: '61762',
+    dialectOptions: {
+        "instanceName": "SQLEXPRESS"
+    },
+    options: {
+        encrypt: true
+    }
+
+})
+
+/* for azure 
  const connection = ({
-     user: 'admin',
-     password: 'evil001',
-     server: 'localhost',
+     user: 'etours',
+     password: "$Son01627335534",
+     server: "etours2.database.windows.net",
      driver: 'tedious',
      database: 'etours',
-     port: '61762',
-     dialectOptions: {
-         "instanceName": "SQLEXPRESS"
-     },
      options: {
          encrypt: true
      }
+ })*/
 
- })
 
- module.exports = dbconfig
+
+module.exports = dbconfig
